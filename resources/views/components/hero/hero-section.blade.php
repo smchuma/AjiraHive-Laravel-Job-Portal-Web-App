56f81f4f02700eff24db193be3b366fbaa23ef5e
smchuma <laurentsamora6@gmail.com>
2023-12-22 16:56:14 +0300

    first commit

---
+++ resources/views/components/hero/hero-section.blade.php
@@ -0,0 +1,17 @@
+<main class="flex bg-[#323640] w-full h-screen ">
+<div class="flex-1 flex justify-cente   ">
+    <div class="mt-20  ">
+        <h1 class=" text-3xl px-10 text-white font-bold " >Connecting Talent with Opportunities <br /> in Tanzania</h1>
+        <p
+        class="mt-5 px-10 "
+        >Are you ready to take the next step in your career or find the perfect candidate for your company? TanzJobs Connect is your go-to platform for tech, accounting, and various job opportunities in Tanzania.</p>
+        <x-button.button class="mx-10 mt-5" >
+            Get Started
+        </x-button.button>
+
+    </div>
+</div>
+<div class="flex-1  ">
+    <img src="/images/ajira.png" alt="ajira" class="mt-20 object-cover" width="600" height="600"   >
+</div>
+</main>
---
+++ resources/views/components/navbar/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="flex justify-between p-5 bg-[#323640] px-10 shadow-lg  ">
+   <div class="logo">
+      <h1 class="text-2xl text-blue-300 ">AjiraHive</h1>
+   </div>
+ <div class="flex items-center gap-5 cursor-pointer " >
+ <p class="underline text-blue-300"  >Login</p>
+   <x-button.button>
+      Register
+     </x-button.button>
+ </div>
+</nav>
